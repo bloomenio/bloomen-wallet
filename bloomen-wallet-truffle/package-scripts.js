5f6a74aee33aab0d0bf3ac04b7ca6bbcf8a9f0af
Jordi Escudero <jordi.escudero@gmail.com>
2019-02-19 15:15:35 +0100

    fix export functionality

--- bloomen-wallet-truffle/package-scripts.js
+++ bloomen-wallet-truffle/package-scripts.js
@@ -4,6 +4,6 @@ require('dotenv').config();
 module.exports = {
   scripts: {
     development: 'truffle migrate --reset',
-    export: 'copyfiles -f build/contracts/*.json ../bloomen-wallet-app/src/providers/services/web3/contracts/json && copyfiles -f build/contracts/*.json ../bloomen-wallet-cli/src/contracts'
+    export: 'copyfiles -f build/contracts/*.json ../bloomen-wallet-app/src/providers/services/web3/contracts/json &&  copyfiles -f build/contracts/*.json ../bloomen-wallet-service-demo/src/providers/services/web3/contracts/json && copyfiles -f build/contracts/*.json ../bloomen-wallet-cli/src/contracts'
   }
 };
