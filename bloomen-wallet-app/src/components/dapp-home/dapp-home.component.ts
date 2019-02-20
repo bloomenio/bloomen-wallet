f6c68ece3fae9a205319bdd9be758d4b27127e5a
EricTorresP <eric.torres@worldline.com>
2019-02-20 17:07:27 +0100

    CHANGE: Buy and Allow logic

--- bloomen-wallet-app/src/components/dapp-home/dapp-home.component.ts
+++ bloomen-wallet-app/src/components/dapp-home/dapp-home.component.ts
@@ -81,51 +81,12 @@ export class DappHomeComponent implements OnInit, OnDestroy {
     this.store.dispatch(new fromTxActivityActions.MoreTxActivity({ page: ++this.currentPage }));
   }
 
-  private buyContentDialog(buyObject) {
-    this.buyObject = buyObject;
-    const dialogRef = this.dialog.open(DappGeneralDialogComponent, {
-      width: '250px',
-      height: '200px',
-      data: {
-        title: this.translate.instant('dapp.notifications.dialog_buy.title'),
-        description: this.translate.instant('dapp.notifications.dialog_buy.description', {
-          id: buyObject.assetId,
-          title: buyObject.description,
-          price: buyObject.amount
-        }),
-        buttonAccept: this.translate.instant('common.accept'),
-        buttonCancel: this.translate.instant('common.cancel')
-      }
-    });
-
-    dialogRef.afterClosed().subscribe(value => {
-      if (value) {
-        this.doBuyTranscation();
-      }
-    });
-  }
-
-  public doBuyTranscation() {
-    this.assets.buy(this.buyObject.assetId, this.buyObject.schemaId, this.buyObject.amount, this.buyObject.dappId, this.buyObject.description)
-      .then((result: any) => {
-        log.debug('OK', result);
-        this.snackBar.open(this.translate.instant('common.transaction_success'), null, {
-          duration: 2000,
-        });
-      }, (error: any) => {
-        log.debug('KO', error);
-        this.snackBar.open(this.translate.instant('common.transaction_error'), null, {
-          duration: 2000,
-        });
-      });
-  }
-
-  public async buyContent() {
+  public async buyOrAllow(){
     if (window['cordova']) {
       try {
         const scannedValue = await this.barCodeScannerService.scan();
         if ((scannedValue) && (!scannedValue.cancelled)) {
-          this.generatePurchase(scannedValue.text);
+          this.doOperation(scannedValue.text);
         } else {
           log.error('KO', 'Scan cancelled');
         }
@@ -138,101 +99,22 @@ export class DappHomeComponent implements OnInit, OnDestroy {
         data: {
           // Juan aqui no mires, jordi me ha dicho que no lo traduzca, lo dejo con el instant para que si algun dia lo tenemos que traducir
           // lo hagamos :(
-          title: this.translate.instant('Buy'),
+          title: this.translate.instant('Buy or Allow'),
           description: this.translate.instant('Put the date into the input field'),
           buttonAccept: this.translate.instant('Ok'),
           buttonCancel: this.translate.instant('Cancel')
         }
       });
-
       dialogRef.afterClosed().subscribe(result => {
-
         if (result) {
-          this.generatePurchase(result);
+          this.doOperation(result);
         }
       });
     }
   }
 
-  private generatePurchase(inputValue: string) {
-    if (inputValue.includes(QR_VALIDATOR.BUY, 0)) {
-      const buyParams = inputValue.replace(QR_VALIDATOR.BUY, '').split('#');
-      const buyObject = {
-        assetId: parseInt(buyParams[0], 10),
-        schemaId: parseInt(buyParams[1], 10),
-        amount: parseInt(buyParams[2], 10),
-        dappId: buyParams[3],
-        description: decodeURI(buyParams[4])
-      };
-      this.buyContentDialog(buyObject);
-    } else {
-      log.error('KO', 'Bad QR prefix');
-      this.snackBar.open(this.translate.instant('common.qr_invalid'), null, {
-        duration: 2000,
-      });
-    }
-  }
 
-  public async allowAccess() {
-    if (window['cordova']) {
-      try {
-        const scannedValue = await this.barCodeScannerService.scan();
-        if ((scannedValue) && (!scannedValue.cancelled)) {
-          this.generteAllow(scannedValue.text);
-        } else {
-          log.error('KO', 'Scan cancelled');
-        }
-      } catch {
-        log.error('Error scanning the QR');
-      }
-    } else {
-      const dialogRef = this.dialog.open(DappInputDialogComponent, {
-        width: '250px',
-        data: {
-          // Juan aqui no mires, jordi me ha dicho que no lo traduzca, lo dejo con el instant para que si algun dia lo tenemos que traducir
-          // lo hagamos :(
-          title: this.translate.instant('Allow'),
-          description: this.translate.instant('Put the date into the input field'),
-          buttonAccept: this.translate.instant('Ok'),
-          buttonCancel: this.translate.instant('Cancel')
-        }
-      });
+  private doOperation(result: any) {
 
-      dialogRef.afterClosed().subscribe(result => {
-
-        if (result) {
-          this.generteAllow(result);
-        }
-      });
-    }
-  }
-
-  private generteAllow(inputValue: string) {
-    if (inputValue.includes(QR_VALIDATOR.ALLOW, 0)) {
-      const allowParams = inputValue.replace(QR_VALIDATOR.ALLOW, '').split('#');
-      const allowObject = {
-        deviceHash: decodeURI(allowParams[0]),
-        assetId: parseInt(allowParams[1], 10),
-        schemaId: parseInt(allowParams[2], 10),
-        dappId: allowParams[3]
-      };
-      this.devices.handshake(allowObject.deviceHash, allowObject.assetId, allowObject.schemaId, allowObject.dappId)
-        .then((result: any) => {
-          log.debug('OK', result);
-          this.snackBar.open(this.translate.instant('common.transaction_success'), null, {
-            duration: 2000,
-          });
-        }, (error: any) => {
-          log.debug('KO', error);
-          this.snackBar.open(this.translate.instant('common.transaction_error'), null, {
-            duration: 2000,
-          });
-        });
-    } else {
-      log.error('KO', 'Bad QR prefix');
-      this.snackBar.open(this.translate.instant('common.qr_invalid'), null, {
-        duration: 2000,
-      });
-    }
   }
 }
