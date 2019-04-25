b605f5427b9cd3a0f84b2be27fa64fba350f630b
Jordi Escudero <jordi.escudero@gmail.com>
2019-04-25 09:28:49 +0200

    add metamask HD_PATH_STRING

--- bloomen-wallet-app/src/core/constants/web3.constants.ts
+++ bloomen-wallet-app/src/core/constants/web3.constants.ts
@@ -4,7 +4,7 @@ export const WEB3_CONSTANTS = {
         INTERACTIVE: 'interactive'
     },
     RANDOM_SEED: 'randomSeed',
-    HD_PATH_STRING: 'm/0\'/0\'/0\'',
+    HD_PATH_STRING: 'm/44\'/60\'/0\'/0',
     BALANCE_TYPE: 'balance',
     DATA_LOADED: 'data_loaded'
 };
