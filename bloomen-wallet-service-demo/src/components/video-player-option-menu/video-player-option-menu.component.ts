b70ffa05c0d8eb31558185eae7d6e6735618c00a
Jordi Escudero <jordi.escudero@gmail.com>
2019-05-27 09:34:38 +0200

    some stuff

--- bloomen-wallet-service-demo/src/components/video-player-option-menu/video-player-option-menu.component.ts
+++ bloomen-wallet-service-demo/src/components/video-player-option-menu/video-player-option-menu.component.ts
@@ -1,18 +1,25 @@
 // Basic
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'blo-video-player-option-menu',
   templateUrl: 'video-player-option-menu.component.html',
   styleUrls: ['video-player-option-menu.component.scss']
 })
-export class VideoPlayerOptionMenuComponent {
+export class VideoPlayerOptionMenuComponent implements OnInit {
+
+  private videoId: string;
 
   constructor(
+    public activatedRoute: ActivatedRoute
   ) {}
+  public ngOnInit() {
+    this.videoId = this.activatedRoute.snapshot.paramMap.get('videoId');
+  }
 
   public doTip() {
-    console.log('doTip...');
+    console.log('doTip...' + this.videoId );
   }
 
 }
